da6466b4da33e6e18d2aaa3835f0d0ac3dc332e8
wdssmq <wdssmq@qq.com>
2026-09-22 23:02:41 +0800

    refactor. 移除不再使用的文件和代码

--- zberOne/include.php
+++ zberOne/include.php
@@ -4,88 +4,34 @@ RegisterPlugin("zberOne", "ActivePlugin_zberOne");
 
 function ActivePlugin_zberOne()
 {
-  Add_Filter_Plugin('Filter_Plugin_Zbp_BuildTemplate', 'zberOne_Temp');
-}
-function zberOne_Temp(&$templates)
-{
-  // 给开发者：不要跨文件闭合 HTML 标签： https://bbs.zblogcn.com/thread-101310.html#484040
-  // 以及 footer 中的 {$footer} 标记也不要省；
-  $templates['footer'] = str_replace('{$footer}', '{$footer}' .  '<script src="' . zberOne_Path("script", "host") . '"></script>', $templates['footer']);
-}
-function zberOne_Path($file, $t = 'path')
-{
-  global $zbp;
-  $result = $zbp->$t . 'zb_users/plugin/zberOne/';
-  switch ($file) {
-    case 'script':
-      return $result . 'script/plugin.js';
-      break;
-    case 'usr':
-      return $result . 'usr/';
-      break;
-    case 'var':
-      return $result . 'var/';
-      break;
-    case 'main':
-      return $result . 'main.php';
-      break;
-    default:
-      return $result . $file;
-  }
-}
-function zberOne_a($href, $title, $text = "")
-{
-  if (empty($text)) {
-    $text = $href;
-  }
-  return "<a href=\"{$href}\" target=\"_blank\" title=\"{$title}\">$text</a>";
-}
-function InstallPlugin_zberOne()
-{
-  global $zbp;
-  $zbp->BuildTemplate();
-}
-function UninstallPlugin_zberOne()
-{
-  global $zbp;
-  $zbp->BuildTemplate();
+
 }
 
-function zberOne_Check()
+
+function InstallPlugin_zberOne()
 {
-  global $zbp;
-  $zberOne = GetVars("zberOne", "COOKIE");
-  if ($zberOne !== "pass") {
-    $href = "{$zbp->host}#zberOne";
-    return zberOne_a($href, "点击获取验证码", "点击获取验证码");
-  }
-  $last = $zbp->config("zberOne")->last;
-  $cur = date("YmdH");
-  if ($last == $cur) {
-    return $zbp->config("zberOne")->data;
-  } else {
-    $rlt = zberOne_Http();
-    if ($rlt == "err") {
-      return "获取失败";
-    }
-    $zbp->config("zberOne")->data = $rlt;
-    $zbp->config("zberOne")->last = $cur;
-    $zbp->SaveConfig('zberOne');
-    return $zbp->config("zberOne")->data;
-  }
+    global $zbp;
+    $zbp->BuildTemplate();
 }
-function zberOne_Http()
+
+function UninstallPlugin_zberOne()
 {
-  $url = "https://api.github.com/repos/zblogcn/zblogphp";
-  $http = Network::Create();
-  $http->open('GET', $url);
-  $http->send();
-  if ($http->status == 200) {
-    $s = $http->responseText;
-    if ($data = json_decode($s)) {
-      $int = 100 + intval($data->watchers / 37);
-      return $int;
-    }
-  }
-  return $http->status;
+    global $zbp;
+    $zbp->BuildTemplate();
 }
+
+// function zberOne_Http()
+// {
+//   $url = "https://api.github.com/repos/zblogcn/zblogphp";
+//   $http = Network::Create();
+//   $http->open('GET', $url);
+//   $http->send();
+//   if ($http->status == 200) {
+//     $s = $http->responseText;
+//     if ($data = json_decode($s)) {
+//       $int = 100 + intval($data->watchers / 37);
+//       return $int;
+//     }
+//   }
+//   return $http->status;
+// }
--- zberOne/main.php
+++ zberOne/main.php
@@ -1,4 +1,5 @@
 <?php
+/** @var string $blogpath */
 require '../../../zb_system/function/c_system_base.php';
 require '../../../zb_system/function/c_system_admin.php';
 $zbp->Load();
@@ -16,30 +17,6 @@ $blogtitle = '一个 zblog 插件';
 require $blogpath . 'zb_system/admin/admin_header.php';
 require $blogpath . 'zb_system/admin/admin_top.php';
 ?>
-<style>
-  p {
-    font-size: 16px;
-  }
-
-  p:not(:last-child) {
-    margin-bottom: .3em;
-  }
-
-  blockquote {
-    padding-left: .7rem;
-    border-left: 3px solid #999;
-    margin-left: .2rem;
-  }
-
-  hr {
-    background-color: #f5f5f5;
-    border: none;
-    display: block;
-    height: 2px;
-    margin: 1.5rem 0;
-    visibility: visible;
-  }
-</style>
 <div id="divMain">
   <div class="divHeader">
     <?php echo $blogtitle; ?>
@@ -47,53 +24,6 @@ require $blogpath . 'zb_system/admin/admin_top.php';
   <div class="SubMenu"></small>
   </div>
   <div id="divMain2">
-    <h3> 一个 QQ 群</h3>
-    <p>群名称：一个 zblog 用户群</p>
-    <p>群 号：859347813</p>
-    <p>用 途：用于账号解封申请</p>
-    <p>链 接：<?php echo zberOne_a("https://jq.qq.com/?_wv=1027&k=Ot3DJ5Xv", "一个 zblog 用户群"); ?></p>
-    <p>验 证：<?php echo zberOne_Check(); ?></p>
-    <p><b>↑↑请自己 Debug 出这个验证码为什么不会显示。</b></p>
-    <hr>
-    <p>
-      插件地址：<?php echo zberOne_a("https://app.zblogcn.com/?id=15575", "一个 zblog 插件"); ?>
-    </p>
-    <blockquote>
-      <h3>代码的重要本质是`证明`，然而人生更主要而又无奈的组成是`认为`！！！</h3>
-      <p>
-        推荐部动画电影：颠倒的帕特玛_番剧_bilibili_哔哩哔哩
-        <a href="https://www.bilibili.com/bangumi/play/ss3668" target="_blank" title="颠倒的帕特玛：第1话_番剧_bilibili_哔哩哔哩">https://www.bilibili.com/bangumi/play/ss3668</a>
-      </p>
-    </blockquote>
-    <hr>
-    <!-- <h3>开发者申请</h3> -->
-    <!-- <p>论坛发贴规范（申请开发者也先看这里）-论坛事务-ZBlogger技术交流中心</p> -->
-    <!-- <p><a href="https://bbs.zblogcn.com/thread-102989.html" target="_blank" title="论坛发贴规范（申请开发者也先看这里）-论坛事务-ZBlogger技术交流中心">https://bbs.zblogcn.com/thread-102989.html</a></p> -->
-    <h3>解封申请</h3>
-    <p>
-      Z-Blog 相关资讯 RSS 订阅
-      <a href="https://bbs.zblogcn.com/thread-100631.html" target="_blank" title="Z-Blog 相关资讯 RSS 订阅">https://bbs.zblogcn.com/thread-100631.html</a> ←使用其他阅读器的可以在这里查看源地址
-    </p>
-    <p><b>↓↓注册并订阅，不需要手机验证；</b></p>
-    <p>
-      zblog 论坛订阅：<?php echo zberOne_a("https://feeds.pub/feed/https%3A%2F%2Fbbs.zblogcn.com%2Findex-0.html%3Frss%3D1", "zblog 论坛"); ?>
-    </p>
-    <p>
-      应用中心订阅：<?php echo zberOne_a("https://feeds.pub/feed/https%3A%2F%2Fapp.zblogcn.com%2Ffeed.php", "应用中心"); ?>
-    </p>
-    <!-- <p>
-      zblog 玩家：<a href="https://www.innoreader.com/bundle/0014cd640d60" target="_blank" title="zblog 玩家">https://www.innoreader.com/bundle/0014cd640d60</a>
-    </p> -->
-    <p><b>↑↑注册并订阅，不需要手机验证；</b></p>
-    <p>----</p>
-    <p>0、大部分只是禁言而非直接踢掉的基本是因为没正确标注 z-blog 版权。<b>你想让人看到你的站，而我确实看到了，并用相应的行动证明我看到了，仅此而已。</b></p>
-    <p>1、注册「feeds.pub」并订阅上边源。（并不限于这个 RSS 阅读器，Feedly，Inoreader 等都可以，手机可以用 Rolly）</p>
-    <p>2、加 QQ 群附图；</p>
-    <p><b>3、因擅自分享应用等原因造成的封号不予解封；</b></p>
-    <p>----</p>
-    <p><b>需附图：（任意 web 或客户端 RSS 阅读器均可）</b></p>
-    <p><img src="doc/001.png" alt="截图"></p>
-  </div>
 </div>
 
 <?php
